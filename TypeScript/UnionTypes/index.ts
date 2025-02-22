f1f2777476ec4742e15c44163efbd86f2f848d93
Danilo <DaniloBrayann@gmail.com>
2025-02-21 21:17:19 -0300

    feat: adicionar função para verificar se o valor é um número e manipulação de botão

--- TypeScript/UnionTypes/index.ts
+++ TypeScript/UnionTypes/index.ts
@@ -2,3 +2,18 @@ let totalValue: string | number = 200;
 totalValue = "abacate"
 
 console.log(totalValue);
+// studying union types
+// studying union types
+function isNumber(value: string | number){
+if(typeof value === 'number'){
+ return true;
+} else{
+     return false;
+}
+}
+console.log(isNumber(200))
+
+
+// studying union types
+const button = document.querySelector('button');
+button?.click();
