062ef33afd2d182a1cbacdfc67b2e0cab59b4ecd
DaniloBrayann <DaniloBrayann@gmail.com>
2025-09-01 10:26:43 -0300

    refactor: ajustar formatação e estrutura da função toNumber

--- TypeScript/UnionTypes/UnionTypes2.ts
+++ TypeScript/UnionTypes/UnionTypes2.ts
@@ -1,7 +1,12 @@
 // exercise: Union Types
 function toNumber(value: number | string) {
-  if(typeof value === 'string'){
-     return parseFloat(value);
+  if (typeof value === 'string') {
+    return parseFloat(value);
+  } else if (typeof value === 'number') {
+    return value;
+  } else {
+    throw "value deve ser um number | string";
   }
-  return value;
 }
+
+console.log(toNumber('300'));
