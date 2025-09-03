a6be22853379f2dba9798ab605d3276c2eb649b5
DaniloBrayann <DaniloBrayann@gmail.com>
2025-09-03 16:33:48 -0300

    feat: adicionar interface e função para exibir informações do produto

---
+++ TypeScript/type e Interface/Types e Interfaces 2.ts
@@ -0,0 +1,27 @@
+//Interface
+interface InterfaceProduto{
+     nome: string;
+     preco:number;
+     teclado: boolean;
+}
+//Usando interface
+type TipoProduto = {
+    nome: string;
+    preco:number;
+    teclado: boolean;
+}
+
+
+
+
+function produto(dados: InterfaceProduto){
+    document.body.innerHTML +`
+    
+    <div>
+    <h2>Nome: ${dados.nome}</h2>
+    <p>${dados.preco}</p>
+    <p>Iclui Teclado: ${dados.teclado ? 'sim' : 'não'}</p>
+    </div>
+    
+    `;
+}
