9ba272b7a66ef52ec311d556b4f4f018a1dcafcd
DaniloBrayann <DaniloBrayann@gmail.com>
2025-09-05 10:35:01 -0300

    feat: adicionar funções para filtrar e exibir valores numéricos

---
+++ TypeScript/Arrays 1/array.ts
@@ -0,0 +1,25 @@
+const numeros = [10, 20, 30, 40, 50];
+const valores = [10, 'Taxas', 30, 'produtos', 50];
+
+
+
+
+function maiorQue10(data: Array<number>) {
+     return data.filter((n) => n > 10);
+}
+
+
+
+function filtrarValores(data: Array<string |number>){
+     return data.filter(item => typeof item === 'number');
+}
+
+console.log(filtrarValores(valores));
+console.log(filtrarValores(numeros));
+
+
+const dados =[
+     ['Gabriel', 30],
+     ['Ana', 25],
+     ['Maria', 28]  
+]
