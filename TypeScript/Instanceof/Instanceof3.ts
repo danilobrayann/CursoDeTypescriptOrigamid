d3d658f2388d64a687edd8781966609caace6fe0
danilobrayann <danilobrayann@gmail.com>
2025-11-25 11:25:06 -0300

    feat: adicionar lógica para definir o href de um link se for um HTMLAnchorElement

---
+++ TypeScript/Instanceof/Instanceof3.ts
@@ -0,0 +1,7 @@
+const link = document.getElementById("origamid");
+
+if (link instanceof HTMLAnchorElement) {
+  /* console.dir(link);*/
+  //ou pode ser usado tbm
+  link.href = "https://www.origamid.com";
+}
