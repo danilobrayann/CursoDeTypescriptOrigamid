a8cb6a4eaed789bf42c7cb84ac9a148fbc9dea86
danilobrayann <danilobrayann@gmail.com>
2025-12-08 10:51:58 -0300

    feat: new ex

--- TypeScript/Instanceof/InterfacesdoDOM1.ts
+++ TypeScript/Instanceof/InterfacesdoDOM1.ts
@@ -2,3 +2,9 @@ const video = document.createElement("video");
 if (video instanceof HTMLVideoElement) {
   console.log(video.volume);
 }
+
+const img = document.createElement("img");
+if (img instanceof HTMLImageElement) {
+  img.src = "imagem.png";
+  console.log(img.src);
+}
